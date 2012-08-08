77c1a4dcb1e36773371f2dfef7d0ce5f90be9a03
Nick Mudge <mudgen@gmail.com>
2012-08-08 16:30:56 -0400

    Updated congomongo library.

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
                  [org.clojure/clojure-contrib "1.2.0"]
                  [ring/ring-jetty-adapter "0.3.11"]
                  [compojure "0.6.5"]
-                 [congomongo "0.1.7-SNAPSHOT"]
+                 [congomongo "0.1.10"]
                  [hiccup "0.3.6"]
                  [gaka "0.2.0"]])
