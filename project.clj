08a469ee78a249d163d77c5cd1316335c29b21f8
Nick Mudge <mudgen@gmail.com>
2012-08-08 16:37:18 -0400

    Removed clojure-contrib

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject jobboard "1.0.0-SNAPSHOT"
   :description "A Compojure jobboard application"
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/clojure-contrib "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.2.1"]               
                  [ring/ring-jetty-adapter "0.3.11"]
                  [compojure "0.6.5"]
                  [congomongo "0.1.10"]
